c3edf0115e7dda886a145caf5ec70bd3ef3e6635
adeeba reaz <adeebarivera@outlook.com>
2020-08-21 01:32:46 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -9,3 +9,22 @@ class Artist
   def songs
     @songs
   end
+
+  def add_song(song)
+    @songs << song
+    song.artist = self
+    @@song_count += 1
+  end
+
+  def add_song_by_name(song_name)
+    song = Song.new(song_name)
+    @songs << song
+    song.artist = self
+    @@song_count += 1
+  end
+
+  def self.song_count
+    @@song_count
+  end
+
+end 
